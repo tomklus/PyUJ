567de5bdcad24ca0aabfdbc0835f30e71451b5c8
Tomas K <t.klus@seznam.cz>
2016-10-29 20:40:43 +0200

    poprawione 4.5

--- Zestaw4/4.5.py
+++ Zestaw4/4.5.py
@@ -3,13 +3,20 @@ def odwracanie(L,left,right):
         x = L[left+i]
         L[left+i] = L[right-i]
         L[right-i] = x 
-   	print L
     return L
 
-def odrwacanie_rek(L.left,right):
+def odwracanie_rek(L,left,right):
+    if left >= right : return L
+    L[left], L[right] = L[right], L[left]
+    return odwracanie_rek(L, left+1, right-1)
     
+        
 
 
 L = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
 
-print odwracanie(L,3,10)
+print odwracanie(L,1,13)
+
+L = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+
+print odwracanie_rek(L,1,13)
