4e2748cb19d644663b5e6c864a54b448767faa9f
Tomas K <t.klus@seznam.cz>
2016-11-03 10:15:19 +0100

    poprawione 4.7

--- Zestaw4/4.7.py
+++ Zestaw4/4.7.py
@@ -2,12 +2,14 @@ def flatten(sequence):
     L = []
     for item in sequence:
         if isinstance(item,(list,tuple)):
-            if len(item) == 0:
-                L.append(flatten(item))
+            if len(item) > 0:
+                L+=flatten(item)
         else:
             L.append(item)
     return L
 
+
 seq = [1,(2,3),[],[4,(5,6,7)],8,[9]]
+
 
 print flatten(seq)
